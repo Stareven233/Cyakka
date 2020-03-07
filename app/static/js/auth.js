0ac2df43743aa60c5dd8ce17892be6192fe9981a
Stareven233 <46963288+Stareven233@users.noreply.github.com>
2020-03-07 18:17:02 +0800

    fix some display bugs

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,1 +1,36 @@
-$(document).ready(function(){if(url.pathname!=='/auth/login')return false;$('#submit').after($('#login'));let login_form=$('.login-box .form');login_form.attr('autocomplete','off');login_form.submit(async function(event){event.preventDefault();let form=new FormData(this);$.getScript('http://pv.sohu.com/cityjson?ie=utf-8',function(){form.set('ip',returnCitySN["cip"]+' '+returnCitySN["cname"]);$.ajax({url:url.href,type:"POST",data:form,processData:false,contentType:false,success:(pre_url)=>window.location=pre_url})})})});$(document).ready(function(){if(url.pathname!=='/auth/register')return false;$('.form').attr('autocomplete','off')});
+// 登录页面 login.html
+$(document).ready(function () {
+    if (url.pathname !== '/auth/login')
+        return false;
+
+    $('#submit').after($('#login'));
+    let login_form = $('.login-box .form');
+    login_form.attr('autocomplete', 'off');
+
+    login_form.submit(async function (event) {
+        event.preventDefault();
+        let form = new FormData(this);
+        $.getScript('http://pv.sohu.com/cityjson?ie=utf-8', function () {
+            form.set('ip', returnCitySN["cip"] + ' '+ returnCitySN["cname"]);
+            $.ajax({
+              url: url.href,
+              type: "POST",
+              data: form,
+              processData: false,  // 不处理数据
+              contentType: false,   // 不设置内容类型
+              success: (doc) => {
+                  if(doc.startsWith('/'))
+                      window.location = doc;
+                  document.querySelector('html').innerHTML = doc;
+              }//不允许$('html').html(doc)
+            });
+        });
+    });
+});
+
+// 注册页面
+$(document).ready(function () {
+    if (url.pathname !== '/auth/register')
+        return false;
+    $('.form').attr('autocomplete', 'off');
+});
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,39 @@
-$(document).ready(function(){if(url.pathname!=='/')return false;let search_btn=$('#c-header .search-box .glyphicon-search');let search_input=$('#search-input');search_btn.click(function(){if(search_input.val())$.post(url.origin+'/search/history',{keyword:search_input.val()});window.open(url.origin+'/search?'+search_input.serialize())});search_input.keydown(function(event){if(event.key==='Enter')search_btn.click()});(function(){$('#c-wrapper .spread-module').hover(function(){$(this).children('p.info').animate({top:'0'},"20")},function(){$(this).children('p.info').animate({top:'-0.28rem'},"20")})})();$('#c-wrapper .wrap-module>header .read-push').click(function(){let div=$(this).data('div');let storey_box=$(`#cya_${div}.storey-box`);$.get(`${url.origin}?div=${div}`,function(res,status){if(status!=='success')return false;storey_box.html(res)})})});
+$(document).ready(function () {
+    if (url.pathname !== '/')
+        return false;
+
+    // header的搜索框
+    let search_btn = $('#c-header .search-box .glyphicon-search');
+    let search_input = $('#search-input');
+
+    search_btn.click(function() {
+        if(search_input.val())
+            $.post(url.origin + '/search/history', {keyword: search_input.val()});
+        window.open( url.origin +  '/search?' + search_input.serialize());
+    });
+    search_input.keydown(function (event) {
+        if(event.key === 'Enter')
+            search_btn.click();
+    });
+
+    (function () {
+        $('#c-wrapper .spread-module').hover(
+        function () {
+            $(this).children('p.info').animate({top:'0'}, "20");
+        },function () {
+            $(this).children('p.info').animate({top:'-0.28rem'}, "20");
+        });
+    })();
+
+    //各分区视频展示的换一换
+    $('#c-wrapper .wrap-module>header .read-push').click(function () {
+        let div = $(this).data('div');
+        let storey_box = $(`#cya_${div} .storey-box`);
+
+        $.get(`${url.origin}?div=${div}`, function (res, status) {
+            if(status !== 'success')
+                return false;
+            storey_box.html(res);
+        })
+    });
+});
